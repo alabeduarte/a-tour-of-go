5ef5b759984b3810416dc4232c9c80414c35e87d
Alabê Duarte <alabeduarte@gmail.com>
2017-07-11 09:12:24 +1000

    Add fibonacci closure exercise

---
+++ moretypes/exercises/exercise-fibonacci-closure.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+  prev := 0
+  curr := 0
+  next := 0
+
+  return func() int {
+    switch {
+    case curr == 0:
+      prev = curr
+      curr++
+      next++
+
+      return curr
+    case curr == 1:
+      prev = curr
+      curr++
+      next++
+
+      return curr
+    default:
+      next = prev + curr
+      prev = curr
+      curr = next
+
+      return curr
+    }
+  }
+}
+
+func main() {
+  f := fibonacci()
+  for i := 0; i < 10; i++ {
+    fmt.Println(f())
+  }
+}
+
